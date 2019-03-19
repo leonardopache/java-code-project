20d637ae6b1f2d24268015cdd3f2ab8a2c2b0883
Leonardo <leonardo@pache.eng.br>
2019-03-19 09:01:22 +0100

    java code

---
+++ src/main/java/pache/all/search/algorithms/LinearSearch.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package pache.all.search.algorithms;
+
+/**
+ * @author lpache
+ *
+ */
+public class LinearSearch {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
