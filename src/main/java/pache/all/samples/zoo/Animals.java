4afda80cd25f9f437d2e2cdd8f961288339cd363
Leonardo <leonardo@pache.eng.br>
2019-07-03 17:39:21 +0200

    zoo

---
+++ src/main/java/pache/all/samples/zoo/Animals.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package pache.all.samples.zoo;
+
+/**
+ * @author lmarquespache
+ *
+ */
+public abstract class Animals {
+
+	protected abstract String favoriteFood();
+	
+	protected abstract String name();
+	
+}
---
+++ src/main/java/pache/all/samples/zoo/TestZoo.java
@@ -0,0 +1,10 @@
+package pache.all.samples.zoo;
+
+public class TestZoo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
